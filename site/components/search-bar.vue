36bcc8504f7e5c19a9610161d1145bc972fab412
stephan-kritzinger <u24581705@tuks.co.za>
2025-05-04 15:26:03 +0200

    Added searchbar component, and accompanying css file

---
+++ site/components/search-bar.vue
@@ -0,0 +1,14 @@
+<template>
+    <label :for="searchLabel">{{ searchLabel }}</label>
+    <input :name="searchLabel">
+</template>
+
+<script setup>
+    const props = defineProps({
+        searchLabel: String
+    })
+</script>
+
+<style scoped>
+    @import "/styles/search-bar.css";
+</style>
--- site/pages/search.vue
+++ site/pages/search.vue
@@ -1,3 +1,13 @@
 <template>
-    e
+    <div>
+        <searchbar :search-label="'Author'" />
+    </div>
+    <div>Or</div>
+    <div>
+        <searchbar :search-label="'Blog Title'" />
+    </div>
 </template>
+
+<script setup>
+    import searchbar from "@/components/search-bar.vue";
+</script>
