e82a0d751e1e4e02b787748efd617dd0aadd6c1a
stephan-kritzinger <u24581705@tuks.co.za>
2025-05-05 17:26:49 +0200

    Removed testing code

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -11,7 +11,6 @@
     </div>
     <div v-if="blogs.length">
         <blog :ref="el => blogList[i] = el" v-for="(blog, i) in blogs" :key="i" :author="blog.author" :title="blog.title" :content="blog.content" :category="blog.category" :id="blog.id" />
-            e
     </div>
 </template>
 
@@ -39,7 +38,6 @@
                 category: article.category.name,
                 id: article.documentId
             });
-            console.log(article);
         }
         const response2 = await fetch("http://localhost:1337/api/categories");
         const data2 = await response2.json();
